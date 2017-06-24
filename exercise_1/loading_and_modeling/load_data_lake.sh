ac6910031b0874a0629df307cd98dcc59cdd0776
CynthiaHu <huyue012@hotmail.com>
2017-06-23 21:43:07 -0700

    update bash script

--- exercise_1/loading_and_modeling/load_data_lake.sh
+++ exercise_1/loading_and_modeling/load_data_lake.sh
@@ -4,8 +4,8 @@
 # Get data into EC2 instance
 mkdir hospital_files
 cd hospital_files/
-wget https://data.medicare.gov/views/bg9k-emty/files/Nqcy71p9Ss2RSBWDmP77H1DQXcyacr2khotGbDHHW_s?content_type=application%2Fzip%3B%20charset%3Dbinary&filename=Hospital_Revised_Flatfiles.zip
-unzip *
+wget -O Hospital_Revised_Flatfiles.zip https://data.medicare.gov/views/bg9k-emty/files/Nqcy71p9Ss2RSBWDmP77H1DQXcyacr2khotGbDHHW_s?content_type=application%2Fzip%3B%20charset%3Dbinary&filename=Hospital_Revised_Flatfiles.zip
+unzip Hospital_Revised_Flatfiles.zip
 ls
 
 # Put relevant files into HDFS

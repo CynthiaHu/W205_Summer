7bb1adde853b4b5644ffca1d2d89ed74baf75a28
CynthiaHu <huyue012@hotmail.com>
2017-06-25 12:41:59 -0700

    try add a blank line

--- exercise_1/loading_and_modeling/load_data_lake.sh
+++ exercise_1/loading_and_modeling/load_data_lake.sh
@@ -7,6 +7,7 @@ cd $HOME
 mkdir hospital_files
 cd hospital_files/
 wget -O Hospital_Revised_Flatfiles.zip http://data.medicare.gov/views/bg9k-emty/files/Nqcy71p9Ss2RSBWDmP77H1DQXcyacr2khotGbDHHW_s?content_type=application%2Fzip%3B%20charset%3Dbinary&filename=Hospital_Revised_Flatfiles.zip
+
 unzip Hospital_Revised_Flatfiles.zip
 # ls
 

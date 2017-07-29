43f5b3089deda7b4fc3aa727915272b77fc9c3e9
Cynthia Hu <huyue012@hotmail.com>
2017-07-29 00:37:07 -0700

    Update Twittercredentials.py

--- exercise_2/Twittercredentials.py
+++ exercise_2/Twittercredentials.py
@@ -1,16 +1,16 @@
 import tweepy
 
-consumer_key = "vCQveddofvBnzmVu5BAuRU7Aq";
+consumer_key = "vCQveddofv******AuRU7Aq";
 #eg: consumer_key = "YisfFjiodKtojtUvW4MSEcPm";
 
 
-consumer_secret = "d15upoch7rsrgzpI6JysSuMEPN0aE0t8yOZ5b5Hr1cjhtbVQTK";
+consumer_secret = "d15upoch7rsrgzp****************5Hr1cjhtbVQTK";
 #eg: consumer_secret = "YisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPm";
 
-access_token = "2287158984-0cPiKsZEBUwJfpCNRZnL4kzvmtBdKTydGMDqHR5";
+access_token = "2287158984-*******************zvmtBdKTydGMDqHR5";
 #eg: access_token = "YisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPm";
 
-access_token_secret = "fwVCAU4qCI6IP1Gcf9g0eRI9BLV4X9Vvoqn9LtI0XpvfZ";
+access_token_secret = "fwVCAU4qCI6IP1*************9Vvoqn9LtI0XpvfZ";
 #eg: access_token_secret = "YisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPm";
 
 
